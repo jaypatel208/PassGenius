6dff9260c00a42396295e601ea7884f00246fbfa
Jay Patel <jay037493@gmail.com>
2024-01-04 19:18:54 +0530

    Basic lazy column component done , need to handle large text in that by clipping or ellipse

--- app/src/main/java/dev/jay/passgenius/ui/components/AppComponents.kt
+++ app/src/main/java/dev/jay/passgenius/ui/components/AppComponents.kt
@@ -2,6 +2,7 @@
 
 package dev.jay.passgenius.ui.components
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,9 +13,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Airlines
+import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,6 +33,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.jay.passgenius.R
@@ -85,7 +91,7 @@ fun TextTotalPasswords(text: String) {
             fontSize = 16.sp,
             fontWeight = FontWeight.Normal
         )
-        
+
         NoTopRectangle(130)
     }
 }
@@ -156,4 +162,54 @@ fun TopBar() {
         },
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = OrangeMetrics)
     )
+}
+
+@Composable
+fun PasswordColumnItem() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .height(50.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(end =12.dp)) {
+            SiteLogo()
+            Column(modifier = Modifier.padding(start = 24.dp)) {
+                Text(
+                    text = "Google.com",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = "leohoward@gmail.com", maxLines = 1, color = Color.Gray)
+            }
+        }
+        Icon(
+            imageVector = Icons.Filled.MoreHoriz,
+            contentDescription = "More Password Column Item",
+            modifier = Modifier.padding(start = 12.dp, end = 8.dp)
+        )
+    }
+}
+
+@Composable
+private fun SiteLogo() {
+    Box(contentAlignment = Alignment.Center) {
+        Canvas(
+            modifier = Modifier
+                .size(size = 50.dp)
+        ) {
+            drawRect(color = Color.Gray)
+        }
+        Icon(imageVector = Icons.Filled.Airlines, contentDescription = "Logo")
+    }
+}
+
+@Composable
+@Preview
+fun PasswordColumnItemPreview() {
+    PasswordColumnItem()
 }
